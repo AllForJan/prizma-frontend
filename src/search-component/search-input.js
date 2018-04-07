97a7f50c5239d6a7dfa958171acdce962a397e7b
Juraj Muransky <juraj.muransky@gmail.com>
2018-04-07 09:57:35 +0200

    first demo of search input

---
+++ src/search-component/search-input.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+class _SearchInput extends React.Component {
+	constructor(props){
+		super(props);
+		this.handleOnSearch=this.handleOnSearch.bind(this);
+		this.state={value:''}
+	}
+	
+	handleOnSearch(evt) {
+		this.setState({value:evt.target.value})
+	}
+	
+	render(){
+		const { value } = this.state
+		return <React.Fragment><label>Search:</label><input type="text" value={value} onChange={this.handleOnSearch} /></React.Fragment>
+	}
+}
+
+export const SearchInput = _SearchInput
